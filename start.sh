87eca8386e02d164ab4fd9b63774b290234ebe4e
JujuDesFruits <julien.trijean@gmail.com>
2019-03-06 12:01:54 +0100

    minor change about file master and slave

--- start.sh
+++ start.sh
@@ -11,11 +11,19 @@ echo "simulating new servers"
 vagrant up
 
 cd $repo
+
+echo "____________________________________________________________________________________________________"
 echo "building first layer for all servers"
+echo "____________________________________________________________________________________________________"
+
 ansible-playbook playbook_base.yml -i ALL --extra-vars "ansible_user=vagrant ansible_password=vagrant"
 
+echo "____________________________________________________________________________________________________"
 echo "building second layer for slaves"
+echo "____________________________________________________________________________________________________"
 ansible-playbook playbook_slave.yml -i SLAVE --extra-vars "ansible_user=hduser ansible_password=hduser ansible_become_pass=hduser"
 
+echo "____________________________________________________________________________________________________"
 echo "building second layer for master"
+echo "____________________________________________________________________________________________________"
 ansible-playbook playbook_master.yml -i MASTER --extra-vars "ansible_user=hduser ansible_password=hduser ansible_become_pass=hduser"
