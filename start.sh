584a07dec424e3ba48ab800c62eba1347c024f40
JujuDesFruits <julien.trijean@gmail.com>
2019-03-06 09:04:04 +0100

    minor script changement

--- start.sh
+++ start.sh
@@ -14,6 +14,8 @@ cd $repo
 echo "building first layer for all servers"
 ansible-playbook playbook_base.yml -i ALL --extra-vars "ansible_user=vagrant ansible_password=vagrant"
 
-ansible-playbook playbook_master.yml -i MASTER --extra-vars "ansible_user=hduser ansible_password=hduser ansible_become_pass=hduser"
-
+echo "building second layer for slaves"
 ansible-playbook playbook_slave.yml -i SLAVE --extra-vars "ansible_user=hduser ansible_password=hduser ansible_become_pass=hduser"
+
+echo "building second layer for master"
+ansible-playbook playbook_master.yml -i MASTER --extra-vars "ansible_user=hduser ansible_password=hduser ansible_become_pass=hduser"
