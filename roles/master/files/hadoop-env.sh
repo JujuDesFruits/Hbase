b1382c7661a0b436a413e5855b5a4f161c6394b0
JujuDesFruits <julien.trijean@gmail.com>
2019-03-07 17:26:38 +0100

    issue from hadoop start all script

--- roles/master/files/hadoop-env.sh
+++ roles/master/files/hadoop-env.sh
@@ -51,7 +51,7 @@
 
 # The java implementation to use. By default, this environment
 # variable is REQUIRED on ALL platforms except OS X!
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/jdk-11.0.2
 
 # Location of Hadoop.  By default, Hadoop will attempt to determine
 # this location based upon its execution path.
--- start.sh
+++ start.sh
@@ -18,6 +18,19 @@ echo "__________________________________________________________________________
 
 ansible-playbook playbook_base.yml -i ALL --extra-vars "ansible_user=vagrant ansible_password=vagrant"
 
+
+sshpass -p 'hduser' scp hduser@192.168.56.101:/home/hduser/.ssh/id_rsa.pub id_rsa_1.pub
+sshpass -p 'hduser' scp hduser@192.168.56.102:/home/hduser/.ssh/id_rsa.pub id_rsa_2.pub
+sshpass -p 'hduser' scp hduser@192.168.56.103:/home/hduser/.ssh/id_rsa.pub id_rsa_3.pub
+touch authorized_keys
+cat id_rsa_1.pub >> authorized_keys
+cat id_rsa_2.pub >> authorized_keys
+cat id_rsa_3.pub >> authorized_keys
+cp authorized_keys roles/master/files/authorized_keys
+cp authorized_keys roles/slave/files/authorized_keys
+rm authorized_keys
+rm id_rsa*
+
 echo "____________________________________________________________________________________________________"
 echo "building second layer for slaves"
 echo "____________________________________________________________________________________________________"
