f00a58f421678c1482c01d5c80fd81a09d7cb702
JujuDesFruits <julien.trijean@gmail.com>
2019-04-26 16:52:48 +0200

    correcting changes but still blocked at java

--- start.sh
+++ start.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-hadoop_download_url= http://www-eu.apache.org/dist/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
-hbase_download_url= http://apache.crihan.fr/dist/hbase/1.4.9/hbase-1.4.9-bin.tar.gz
-htrace_download_url= http://repo1.maven.org/maven2/org/apache/htrace/htrace-core/3.1.0-incubating/htrace-core-3.1.0-incubating.jar
+hadoop_download_url=http://www-eu.apache.org/dist/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
+hbase_download_url=http://apache.crihan.fr/dist/hbase/1.4.9/hbase-1.4.9-bin.tar.gz
+htrace_download_url=http://repo1.maven.org/maven2/org/apache/htrace/htrace-core/3.1.0-incubating/htrace-core-3.1.0-incubating.jar
 tmp=0
 
-
-function creator() {
+creator () {
   ###### TO RESET SSH ########
   ssh-keygen -f "/home/vstk8105/.ssh/known_hosts" -R "192.168.56.101"
   ssh-keygen -f "/home/vstk8105/.ssh/known_hosts" -R "192.168.56.102"
@@ -28,10 +27,10 @@ function creator() {
 
   echo "____________________________________________________________________________________________________"
   echo "building layer ssh"
-  echo "__________  echo ""__________________________________________________________________________________________"
-  scp vagrant@192.168.56.101:/home/hadoop/.ssh/id_rsa.pub id_rsa_1.pub
-  scp vagrant@192.168.56.102:/home/hadoop/.ssh/id_rsa.pub id_rsa_2.pub
-  scp vagrant@192.168.56.103:/home/hadoop/.ssh/id_rsa.pub id_rsa_3.pub
+  echo "____________________________________________________________________________________________________"
+  scp vagrant@192.168.56.101:/home/vagrant/.ssh/id_rsa.pub id_rsa_1.pub
+  scp vagrant@192.168.56.102:/home/vagrant/.ssh/id_rsa.pub id_rsa_2.pub
+  scp vagrant@192.168.56.103:/home/vagrant/.ssh/id_rsa.pub id_rsa_3.pub
   touch authorized_keys
   cat ~/.ssh/id_rsa.pub >> authorized_keys
   cat id_rsa_1.pub >> authorized_keys
@@ -44,19 +43,18 @@ function creator() {
   ansible-playbook playbook_ssh.yml -i hosts-dev
 }
 
-
-if [! -d "roles/common/templates/hadoop-3.1.2.tar.gz"]
+if ! test -e roles/common/templates/hadoop-3.1.2.tar.gz ;
 then
   echo "Some Packages are needed for ansible to install hadoop and hbase"
-  while [ $tmp == 0 ]; do
+  while test $tmp -eq 0 ; do
     read -p "These will take over 3GB internet, do you agree to download them now ? (Yes/No)" answer
-	  if [[ "$answer" == "YES" || "$answer" == "yes" || "$answer" == "Yes" || "$answer" == "Y" || 		"$answer" == "y" ]];
+	  if test "$answer" = "YES" || test "$answer" = "yes" || test "$answer" = "Yes" || test "$answer" = "Y" || test "$answer" = "y" ;
 	  then
 	     wget $hadoop_download_url -P roles/common/templates/
        wget $hbase_download_url -P roles/hbase/templates/
-       creator()
+       creator
 		   tmp=$(($tmp + 1))
-    elif [[ "$answer" == "NO" || "$answer" == "no" || "$answer" == "No" || "$answer" == "N" || 			"$answer" == "n" ]];
+    elif test "$answer" = "NO" || test "$answer" = "no" || test "$answer" = "No" || test "$answer" = "N" || test "$answer" = "n" ;
 	  then
 		    echo "come back when you're ready to download them"
 		    tmp=$(($tmp + 1))
@@ -65,5 +63,5 @@ then
 	  fi
   done;
 else
-creator()
+creator
 fi
